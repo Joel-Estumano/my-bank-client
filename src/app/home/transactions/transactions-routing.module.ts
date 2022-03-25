101fe210513ced30ff60eb55498cc99d00f9dd0f
Carolina Xavier <carolina.santos-pla@parceirosedu.com.br>
2022-03-25 18:26:25 -0300

    operations add

--- src/app/home/transactions/transactions-routing.module.ts
+++ src/app/home/transactions/transactions-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OperationsComponent } from './views/operations/operations.component';
 import { TransactionsComponent } from './views/transactions/transactions.component';
 
 const routes: Routes = [
-    { path: '', component: TransactionsComponent }
+    { path: '', component: TransactionsComponent },
+    { path: 'operations', component:OperationsComponent}
 ];
 
 @NgModule({
