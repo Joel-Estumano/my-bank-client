b3bb8e7b3ffc338910abc57f272f1c0fdae01c7a
Carolina Xavier <carolina.xavier@wizecompany.com.br>
2022-03-25 10:35:56 -0300

    fix

--- src/app/home/transactions/transactions.module.ts
+++ src/app/home/transactions/transactions.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TransactionsComponent } from './views/transactions/transactions.component';
 import { TransactionsRoutingModule } from './transactions-routing.module';
+import { TransactionsService } from './services/transactions.service';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,9 @@ import { TransactionsRoutingModule } from './transactions-routing.module';
   imports: [
     CommonModule,
     TransactionsRoutingModule
+  ],
+  providers: [
+    TransactionsService
   ]
 })
 export class TransactionsModule { }
